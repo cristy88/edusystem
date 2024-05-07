43dbbe196c620d0587403503f84f34f107dd0b52
YUE <1311655997@qq.com>
2024-05-07 23:45:11 +0800

    引用组件

--- src/pages/home/examination/list/List.jsx
+++ src/pages/home/examination/list/List.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getExaninationListApi } from '../../../../api'
+import { Space, Table, Tag } from 'antd'
 
 const List = () => {
   const [list, setList] = useState([])
@@ -9,11 +10,100 @@ const List = () => {
       setList(res.data.data.list)
     }
   }
+
+
+  const columns = [
+    {
+      title: '考试名称',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: '科目分类',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '创建者',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '创建时间',
+      key: 'tags',
+      dataIndex: 'tags',
+    },
+    {
+      title: '时间',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '监考人',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '考试班级',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '开始时间',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '结束时间',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '设置',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+    {
+      title: '操作',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ]
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ]
+
+  
   useEffect(() => {
     getExamination()
   }, [])
 
-  console.log(list)
   return (
     <div>
       {list.map(item => 
@@ -24,6 +114,7 @@ const List = () => {
           {item.classify}
         </div>
       )}
+      <Table columns={columns} dataSource={data} />
     </div>
   )
 }
