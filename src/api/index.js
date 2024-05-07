93c9d7bb9baf19f84f130df4776eaf60c7d32b44
LYR <lyr04202022@163.com>
2024-05-07 15:41:53 +0800

    侧边栏

--- src/api/index.js
+++ src/api/index.js
@@ -88,3 +88,20 @@ export const toAvatarApi = ({avatar}) => {
     avatar
   })
 }
+
+// 修改用户信息
+export const updateInfoApi = ({username, password, sex, age, email, avatar}) => {
+  axios.post('user/update/info', {
+    username,
+    password,
+    sex,
+    age,
+    email,
+    avatar
+  })
+}
+
+// 查询左侧菜单
+export const getLeftMeauApi = () => {
+  return axios.get('/user/menulist')
+}
